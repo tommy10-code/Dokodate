8e86cae653a7e918c3779dfa42026eaaffce68be
tommy10-code <mtywp058@gmail.com>
2025-12-14 15:49:42 +0900

    ゲストログイン追加

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,12 @@
 class HomeController < ApplicationController
   def index; end
+
+  def guest_login
+    user = User.find_by!(email: "guest@example.com")
+    sign_in user
+    redirect_to authenticated_root_path, notice: "ゲストでログインしました"
+  end
+
   def terms; end
   def privacy; end
 end
